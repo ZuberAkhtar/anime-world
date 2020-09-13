707cd4e7bf9df59badac8d77c6e5ac0fd561e016
ZuberAkhtar <zuberakhtar@gmail.com>
2020-09-13 20:09:43 +0530

    Updates

---
+++ precache-manifest.2d6c2d1bc8e3c75f4296c171e4aafab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8301f4f5eedfcfb514f4263ea9d5e7e",
+    "url": "/anime-world/index.html"
+  },
+  {
+    "revision": "107d812480f99c38fd62",
+    "url": "/anime-world/static/css/main.02d56b56.chunk.css"
+  },
+  {
+    "revision": "374a518a9316a3bd2288",
+    "url": "/anime-world/static/js/2.8d445ee0.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/anime-world/static/js/2.8d445ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107d812480f99c38fd62",
+    "url": "/anime-world/static/js/main.719d770b.chunk.js"
+  },
+  {
+    "revision": "d121423bdb3ffbf67221",
+    "url": "/anime-world/static/js/runtime-main.9a0eaf15.js"
+  }
+]);
